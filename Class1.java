7e845e931b03573a668f364f5e89a52f603b2f99
Sangit001 <sangit1543@gmail.com>
2026-01-29 22:30:28 +0545

    Logic Building Class1

---
+++ Class1.java
@@ -0,0 +1,24 @@
+
+/*
+        If we mode(%) any number with 10 then the reminder
+        will always give its last number.
+
+        For Example:-   System.out.println(105%10); Answer is 5
+
+        If any number divide(/) with 10 then the answer will always
+        give front number except last one.
+
+        For Example:-   System.out.println(105/10); Answer is 10
+
+
+*/
+
+//  Write a program to print the last number of 1054 and print the first number.
+
+        public class Class1{
+            public static void main(String[] args){
+               int number = 1054;
+               System.out.println("The last number of "+number+ "is " +1054%10);
+            }
+        }
+
