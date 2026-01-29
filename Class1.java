3bebf88d33556cc203ad302a39699aaaded143ac
Sangit001 <sangit1543@gmail.com>
2026-01-29 22:45:35 +0545

    Print first and last number from any number

--- Class1.java
+++ Class1.java
@@ -17,8 +17,14 @@
 
         public class Class1{
             public static void main(String[] args){
-               int number = 1054;
+               int number = 3054;
                System.out.println("The last number of "+number+ "is " +1054%10);
+               
+               // To print the first number Divide by 10
+               int convert = (number/100);
+            //    divided 1054 by 10 and answer is 10
+               int finalResult = (convert/10);
+               System.out.println("The first number of "+ number + " is" + finalResult);
             }
         }
 
