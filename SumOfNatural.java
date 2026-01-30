94670bbabb03dcc8028990781fbf0c9840ae988a
Sangit001 <sangit1543@gmail.com>
2026-01-30 22:38:04 +0545

    Sum of 10 natural number

---
+++ SumOfNatural.java
@@ -0,0 +1,13 @@
+public class SumOfNatural{
+    public static void main(String[] args){
+
+        // sum of 10 natural number
+        int number = 10;
+        int sum = 0;
+        for(int i = 0; i<=number; i++){
+            sum += i;
+        }
+        System.out.println("The sum of "+number+" natural number is "+sum );
+
+    }
+}
