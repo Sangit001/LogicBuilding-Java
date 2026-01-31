9f25e7ff501d31f1ceb9b86ce3a2eb253d92d441
Sangit001 <sangit1543@gmail.com>
2026-01-31 22:28:35 +0545

    SwapNumber

---
+++ SwapNumber.java
@@ -0,0 +1,14 @@
+public class SwapNumber{
+    public static void main(String[] args){
+        
+        // Initialization 
+        int num1 = 10;
+        int num2 = 20;
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println(num1);
+        System.out.println(num2); 
+    }
+}
