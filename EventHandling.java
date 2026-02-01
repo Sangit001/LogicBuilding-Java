dd183d799bcf5984e5fd5e56c1b246e111a39421
Sangit001 <sangit1543@gmail.com>
2026-02-01 23:34:14 +0545

    replaced

--- EventHandling.java
+++ EventHandling.java
@@ -1,71 +1,74 @@
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Eventhandling extends JFrame implements ActionListener{
+public class Eventhandling extends JFrame implements ActionListener {
+
     JTextField resultValue;
-    JTextField tf1 ;
+    JTextField tf1;
     JTextField tf2;
     JButton addBtn;
     JButton subBtn;
-    Eventhandling (){
+
+    Eventhandling() {
+
         JLabel fValue = new JLabel("First Value");
         fValue.setBounds(30,40,100,20);
         add(fValue);
+
         JLabel sValue = new JLabel("Second Value");
         sValue.setBounds(30,70,100,20);
         add(sValue);
+
         JLabel result = new JLabel("Result");
         result.setBounds(30,100,100,20);
         add(result);
 
-        // For text field
-        JTextField tf1 = new JTextField();
+        tf1 = new JTextField();
         tf1.setBounds(120,40,100,20);
         add(tf1);
 
-        JTextField tf2 = new JTextField();
+        tf2 = new JTextField();
         tf2.setBounds(120,70,100,20);
         add(tf2);
 
-        JTextField resultValue = new JTextField();
+        resultValue = new JTextField();
         resultValue.setBounds(120,100,100,20);
         add(resultValue);
-        // Buttons
 
-        JButton addBtn = new JButton("Add");
+        addBtn = new JButton("Add");
         addBtn.setBounds(30,130,100,20);
-        add(addBtn); 
-        JButton subBtn = new JButton("Subtract");
+        add(addBtn);
+
+        subBtn = new JButton("Subtract");
         subBtn.setBounds(130,130,100,20);
-        add(subBtn); 
+        add(subBtn);
+
         addBtn.addActionListener(this);
         subBtn.addActionListener(this);
-
 
         setTitle("Action Event Handling");
         setSize(400,300);
         setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent ae){
-        int x,y,z=0;
-        x = Integer.parseInt(tf1.getText());
-        y = Integer.parseInt(tf2.getText());
+    public void actionPerformed(ActionEvent ae) {
+        int x = Integer.parseInt(tf1.getText());
+        int y = Integer.parseInt(tf2.getText());
+        int z = 0;
 
-        if(addBtn.getActionCommand()== "Add"){
-            z = x+y;
+        if (ae.getActionCommand().equals("Add")) {
+            z = x + y;
         }
-        if(subBtn.getActionCommand() == "Subtract"){
+        if (ae.getActionCommand().equals("Subtract")) {
             z = x - y;
         }
+
         resultValue.setText(String.valueOf(z));
     }
 
-    public static void main(String[] args){
-        Eventhandling eh = new Eventhandling();
-
-
-    
+    public static void main(String[] args) {
+        new Eventhandling();
     }
 }
