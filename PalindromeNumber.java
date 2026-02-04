92f6601e3931222da69a3ee7dafd872f134b3cb5
Sangit001 <sangit1543@gmail.com>
2026-02-04 23:15:46 +0545

    Input from user

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class PalindromeNumber {
     public static void main(String[] args) {
-        int num = 121;
+
+        // Input from user
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         int original = num;
         int rev = 0;
 
